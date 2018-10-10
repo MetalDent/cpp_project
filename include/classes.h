4bd86cae762bdb70a055b29c1622da41af6b9dbc
sudonitesh <nktosni@gmail.com>
2018-10-10 20:13:24 +0530

    added boilerplate

---
+++ include/classes.h
@@ -0,0 +1,13 @@
+#ifndef CLASSES_H
+#define CLASSES_H
+
+class Demo
+{
+    private:
+        int i;
+    public:
+        Demo(int n);
+        int showData();
+};
+
+#endif
---
+++ src/functions.cpp
@@ -0,0 +1,10 @@
+#include "../include/functions.h"
+#include <iostream>
+
+using namespace std;
+
+int fooFun()
+{
+    double a=100;
+    return a;
+}
