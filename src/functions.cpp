2a7db4b2d2bcdb897f2709bdb6e7aa3c5cf0f296
MetalDent <37808619+MetalDent@users.noreply.github.com>
2018-10-30 23:53:09 +0530

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -14,3 +14,23 @@ int fooFun()
     double a=100;
     return a;
 }
+
+vector<vector<string>> get_data()
+{
+	string file_name = "../database/db.csv", delimeter = ",";
+
+	ifstream file(file_name);
+	vector<vector<string>> dataList;
+	string line = "";
+
+	while(getline(file, line))
+	{
+		vector<string> vect;
+		boost::algorithm::split(vect, line, boost::is_any_of(delimeter));
+		dataList.push_back(vect);
+	}
+
+	file.close();
+
+	return dataList;
+}
