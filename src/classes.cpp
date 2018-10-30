4f6e052b9ea6149c665bde3e3946af224baee43b
MetalDent <37808619+MetalDent@users.noreply.github.com>
2018-10-30 23:48:24 +0530

    Update classes.cpp

--- src/classes.cpp
+++ src/classes.cpp
@@ -1,5 +1,11 @@
 #include "../include/classes.h"
 #include <iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<boost/algorithm/string.hpp>
 
 using namespace std;
 
